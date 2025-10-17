893c25d3ebbeaf03fbd6101334a96142b9242006
Sascha <156184267+Sascha37@users.noreply.github.com>
2025-10-18 00:08:20 +0200

    Added GUI

---
+++ src/super-mario-motion/gui.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+from pathlib import Path
+
+height = 650
+width = 700
+background_color = '#141618'
+
+default_image_path = Path(__file__).parent / "data" / 'webcam_sample.jpg'
+
+default_image_top_padding = 50
+
+def init():
+    window = tk.Tk()
+
+    window.title('Super Mario Motion')
+    window.minsize(height,width)
+    window.maxsize(height,width)
+
+    window.configure(background=background_color)
+
+    default_image = ImageTk.PhotoImage(Image.open(default_image_path))
+    image_panel = tk.Label(window, image = default_image)
+    image_panel.pack(pady = default_image_top_padding)
+
+    window.mainloop()
--- src/super-mario-motion/main.py
+++ src/super-mario-motion/main.py
@@ -2,4 +2,7 @@ import tkinter as tk
 import mediapipe as mp
 import cv2
 
+import gui
+
 print("hello world")
+gui.init()
