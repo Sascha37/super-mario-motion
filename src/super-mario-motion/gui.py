e0829f956d7eb9047276e28570e5dc21cc7bd14e
Sascha <156184267+Sascha37@users.noreply.github.com>
2025-10-18 17:22:52 +0200

    Added more images for poses

--- src/super-mario-motion/gui.py
+++ src/super-mario-motion/gui.py
@@ -2,12 +2,14 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from pathlib import Path
 
+# Height and Width
 window_width = 650
 window_height = 700
 
 webcam_image_width = 612
 webcam_image_height = 408
 
+# Colors
 color_background = '#202326'
 color_foreground = '#141618'
 color_white = '#FFFFFF'
@@ -17,9 +19,11 @@ path_data_folder = Path(__file__).parent / "data"
 path_image_webcam_sample = path_data_folder / 'webcam_sample.jpg'
 path_image_walking = path_data_folder / 'walking.png'
 
+# Paddings
 default_image_top_padding = 50
-checkbox_left_padding = 20
+x_padding = 20
 
+# Function gets called once in main.py once the program starts
 def init():
     global window     
     window = tk.Tk()
@@ -33,7 +37,10 @@ def init():
     image_webcam_sample = ImageTk.PhotoImage(Image.open(path_image_webcam_sample).resize((webcam_image_width,webcam_image_height), Image.LANCZOS))
     image_walking = ImageTk.PhotoImage(Image.open(path_image_walking).resize((100,100), Image.LANCZOS))
 
-    label_webcam = tk.Label(window, image = image_webcam_sample)
+    label_webcam = tk.Label(
+        window,
+        image = image_webcam_sample,
+        bd=0)
     label_webcam.image = image_webcam_sample
     label_webcam.pack(pady = default_image_top_padding)
 
@@ -42,8 +49,16 @@ def init():
     global send_keystrokes
     send_keystrokes = tk.IntVar()
 
-    checkbox_toggle_skeleton = tk.Checkbutton(
+    frame_checkboxes = tk.Frame(
         window,
+        bg=color_foreground)
+
+    frame_checkboxes.pack(
+        side='left',
+        padx = x_padding
+    )
+    checkbox_toggle_skeleton = tk.Checkbutton(
+        frame_checkboxes,
         text='Enable Skeleton',
         bg = color_foreground,
         fg = color_white,
@@ -53,11 +68,13 @@ def init():
         variable=skeleton_active,
         onvalue=1,
         offvalue=0,
-        width=20)
-    checkbox_toggle_skeleton.pack(anchor='w', padx= checkbox_left_padding)
+        width=20,
+        height=2)
+    checkbox_toggle_skeleton.pack(
+        anchor='w')
 
     checkbox_toggle_inputs = tk.Checkbutton(
-        window,
+        frame_checkboxes,
         text='Send Inputs',
         bg = color_foreground,
         fg = color_white,
@@ -67,21 +84,28 @@ def init():
         variable=send_keystrokes,
         onvalue=1,
         offvalue=0,
-        width=20)
-    checkbox_toggle_inputs.pack(anchor='w', padx= checkbox_left_padding)
+        width=20,
+        height=2)
 
+    checkbox_toggle_inputs.pack(
+        anchor= 'w',)
 
-    label_pose_visualizer = tk.Label(window, image=image_walking)
+    label_pose_visualizer = tk.Label(
+        window,
+        image=image_walking,
+        bd=0)
     label_pose_visualizer.image = image_walking
-    label_pose_visualizer.pack()
+    label_pose_visualizer.pack(
+        side='right'
+        , padx=x_padding)
     
-    print(ImageTk.PhotoImage(Image.open(path_image_webcam_sample)).height(), ImageTk.PhotoImage(Image.open(path_image_webcam_sample)).width())
-
     print(Path(__file__).name + " initialized")
 
+# Both functions will be called in a loop in main.py,
+# updating the respective image labels with new information from opencv
 def update_label_webcam(new_image):
     image = ImageTk.PhotoImage(new_image.resize((300,400), Image.LANCZOS))
     window.label_webcam = tk.Label(window, image = image)
 
-def update_label_pose_visualizer():
+def update_label_pose_visualizer(pose):
     print("update_label_pose_visualizer")
