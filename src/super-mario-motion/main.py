bf43875882d3ca80c8817c2b2a4f7fc5ee4f5dda
TimoB05 <timo.barton@student.uni-tuebingen.de>
2025-10-19 14:49:38 +0200

    called gui.update_image_panel(frame) in main

--- src/super-mario-motion/main.py
+++ src/super-mario-motion/main.py
@@ -3,7 +3,6 @@ import vision
 
 # Function gets called every millisecond after the mainloop of the tkinter ui
 def update():
-    # TODO: Grab the newest output from vision.py and call gui.window.update_image_panel()
     rgb = vision.get_latest_raw_frame()
     frame = vision.get_latest_skeleton()
     gui.update_image_panel(frame)
