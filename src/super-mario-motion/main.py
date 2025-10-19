72b53783e4ecb9b9c054f5b6386b6873ac148453
timo-barton <timo.barton1@gmail.com>
2025-10-19 14:44:16 +0200

    calles gui.update_image_panel(frame) in main

--- src/super-mario-motion/main.py
+++ src/super-mario-motion/main.py
@@ -6,6 +6,7 @@ def update():
     # TODO: Grab the newest output from vision.py and call gui.window.update_image_panel()
     rgb = vision.get_latest_raw_frame()
     frame = vision.get_latest_skeleton()
+    gui.update_image_panel(frame)
 
     gui.window.after(1,update)
 
