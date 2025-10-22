9ab199fcbe4c7f025cbed540cf64303abc70db6d
silasleo-dev <silas.jirikovsky@student.uni-tuebingen.de>
2025-10-22 13:07:05 +0200

    Add support for eye recognition and split walking into walking and running

--- src/super-mario-motion/vision.py
+++ src/super-mario-motion/vision.py
@@ -20,12 +20,14 @@ mpPose = mp.solutions.pose
 mpDrawing = mp.solutions.drawing_utils
 
 # landmark indices
-left_shoulder = 11
-right_shoulder = 12
-left_elbow = 13
-right_elbow = 14
-left_wrist = 15
-right_wrist = 16
+eye_left = 2
+eye_right = 5
+shoulder_left = 11
+shoulder_right = 12
+elbow_left = 13
+elbow_right = 14
+wrist_left = 15
+wrist_right = 16
 hip_left = 23
 hip_right = 24
 knee_left = 25
@@ -87,12 +89,14 @@ def cam_loop():
                 lm = results.pose_landmarks.landmark
 
                 # get joint coordinates
-                left_shoulder_x, left_shoulder_y = landmark_coords(frame, lm[left_shoulder])
-                right_shoulder_x, right_shoulder_y = landmark_coords(frame, lm[right_shoulder])
-                left_elbow_x, left_elbow_y = landmark_coords(frame, lm[left_elbow])
-                right_elbow_x, right_elbow_y = landmark_coords(frame, lm[right_elbow])
-                left_wrist_x, left_wrist_y = landmark_coords(frame, lm[left_wrist])
-                right_wrist_x, right_wrist_y = landmark_coords(frame, lm[right_wrist])
+                eye_left_x, eye_left_y = landmark_coords(frame, lm[eye_left])
+                eye_right_x, eye_right_y = landmark_coords(frame, lm[eye_right])
+                shoulder_left_x, shoulder_left_y = landmark_coords(frame, lm[shoulder_left])
+                shoulder_right_x, shoulder_right_y = landmark_coords(frame, lm[shoulder_right])
+                elbow_left_x, elbow_left_y = landmark_coords(frame, lm[elbow_left])
+                elbow_right_x, elbow_right_y = landmark_coords(frame, lm[elbow_right])
+                wrist_left_x, wrist_left_y = landmark_coords(frame, lm[wrist_left])
+                wrist_right_x, wrist_right_y = landmark_coords(frame, lm[wrist_right])
                 hip_left_x, hip_left_y = landmark_coords(frame, lm[hip_left])
                 hip_right_x, hip_right_y = landmark_coords(frame, lm[hip_right])
                 knee_left_x, knee_left_y = landmark_coords(frame, lm[knee_left])
@@ -100,21 +104,21 @@ def cam_loop():
                 ankle_left_x, ankle_left_y = landmark_coords(frame, lm[ankle_left])
                 ankle_right_x, ankle_right_y = landmark_coords(frame, lm[ankle_right])
 
-                right_hand_up = right_wrist_y < right_shoulder_y  # check if right wrist is above right shoulder
-                left_hand_up = left_wrist_y < left_shoulder_y     # check if left wrist is above left shoulder
-                both_hands_up = right_hand_up and left_hand_up
-                hands_below_knees = right_wrist_y < knee_right_y and left_wrist_y < knee_left_y
+                running_right = wrist_right_y < shoulder_right_y and wrist_right_y > eye_right_y  # check if right wrist is above right shoulder
+                running_left = wrist_left_y < shoulder_left_y and wrist_left_y > eye_left_y         # check if left wrist is above left shoulder
+                both_hands_up = running_right and running_left
+                hands_below_knees = wrist_right_y < knee_right_y and wrist_left_y < knee_left_y
 
 
                 if both_hands_up:
                     print("Both hands up!")
                     current_pose = "jumping"
-                elif right_hand_up:
+                elif running_right:
                     print("Right hand up!")  # debug message
-                    current_pose = "walking_right"
-                elif left_hand_up:
+                    current_pose = "running_right"
+                elif running_left:
                     print("Left hand up!")
-                    current_pose = "walking_left"
+                    current_pose = "running_left"
                 elif hands_below_knees:
                     print("Both hands below knees!")
                     current_pose = "crouching"
