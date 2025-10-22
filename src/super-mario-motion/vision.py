c653c7bcd81f39f6ee0120b3df32e4769127929f
silasleo-dev <silas.jirikovsky@student.uni-tuebingen.de>
2025-10-22 12:35:44 +0200

    Refactor pose detection and add 3 new poses

--- src/super-mario-motion/vision.py
+++ src/super-mario-motion/vision.py
@@ -19,6 +19,19 @@ frame = None
 mpPose = mp.solutions.pose
 mpDrawing = mp.solutions.drawing_utils
 
+# landmark indices
+left_shoulder = 11
+right_shoulder = 12
+left_elbow = 13
+right_elbow = 14
+left_wrist = 15
+right_wrist = 16
+hip_left = 23
+hip_right = 24
+knee_left = 25
+knee_right = 26
+ankle_left = 27
+ankle_right = 28
 
 def landmark_coords(image, lm):
     h = image.shape[0]
@@ -73,19 +86,38 @@ def cam_loop():
 
                 lm = results.pose_landmarks.landmark
 
-                # landmark indices
-                right_wrist = 16
-                right_shoulder = 12
+                # get joint coordinates
+                left_shoulder_x, left_shoulder_y = landmark_coords(frame, lm[left_shoulder])
+                right_shoulder_x, right_shoulder_y = landmark_coords(frame, lm[right_shoulder])
+                left_elbow_x, left_elbow_y = landmark_coords(frame, lm[left_elbow])
+                right_elbow_x, right_elbow_y = landmark_coords(frame, lm[right_elbow])
+                left_wrist_x, left_wrist_y = landmark_coords(frame, lm[left_wrist])
+                right_wrist_x, right_wrist_y = landmark_coords(frame, lm[right_wrist])
+                hip_left_x, hip_left_y = landmark_coords(frame, lm[hip_left])
+                hip_right_x, hip_right_y = landmark_coords(frame, lm[hip_right])
+                knee_left_x, knee_left_y = landmark_coords(frame, lm[knee_left])
+                knee_right_x, knee_right_y = landmark_coords(frame, lm[knee_right])
+                ankle_left_x, ankle_left_y = landmark_coords(frame, lm[ankle_left])
+                ankle_right_x, ankle_right_y = landmark_coords(frame, lm[ankle_right])
 
-                # get right wrist and right shoulder coordinates
-                rw_x, rw_y = landmark_coords(frame, lm[right_wrist])
-                rs_x, rs_y = landmark_coords(frame, lm[right_shoulder])
+                right_hand_up = right_wrist_y < right_shoulder_y  # check if right wrist is above right shoulder
+                left_hand_up = left_wrist_y < left_shoulder_y     # check if left wrist is above left shoulder
+                both_hands_up = right_hand_up and left_hand_up
+                hands_below_knees = right_wrist_y < knee_right_y and left_wrist_y < knee_left_y
 
-                right_hand_up = rw_y < rs_y  # check if right wrist is above right shoulder
 
-                if right_hand_up:
+                if both_hands_up:
+                    print("Both hands up!")
+                    current_pose = "jumping"
+                elif right_hand_up:
                     print("Right hand up!")  # debug message
                     current_pose = "walking_right"
+                elif left_hand_up:
+                    print("Left hand up!")
+                    current_pose = "walking_left"
+                elif hands_below_knees:
+                    print("Both hands below knees!")
+                    current_pose = "crouching"
                 else:
                     current_pose = "standing"
             if exitApp:
