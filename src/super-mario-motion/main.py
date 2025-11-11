a152e9c53515f08f47050be743759f9c4675c8e7
Sascha <156184267+Sascha37@users.noreply.github.com>
2025-11-11 21:49:18 +0100

    removed duplicate code in main

--- src/super-mario-motion/main.py
+++ src/super-mario-motion/main.py
@@ -3,7 +3,6 @@ import gui
 import vision
 import vision_ml
 import input
-import argparse
 import collect
 import sys
 from state import StateManager
@@ -40,46 +39,18 @@ def update():
     gui.window.after(1, update)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--collect', action='store_true', help='Starte den Datensammler')
-    parser.add_argument('--label', type=str, help='Label für Datensammlung')
-    parser.add_argument('--seconds', type=float, help='Dauer der Aufnahme')
-    parser.add_argument('--csv', type=str, help='Pfad zur CSV-Datei')
-    parser.add_argument('--fps', type=float, help='Samplingrate')
-    parser.add_argument('--source', type=str, choices=['auto', 'vision', 'camera'], help='Frame-Quelle')
-    parser.add_argument('--camera-index', type=int, help='Kameraindex')
-    args = parser.parse_args()
-
-    if args.collect:
-        new_argv = ['collect.py']
-        if args.label is not None:
-            new_argv += ['--label', args.label]
-        if args.seconds is not None:
-            new_argv += ['--seconds', str(args.seconds)]
-        if args.csv is not None:
-            new_argv += ['--csv', args.csv]
-        if args.fps is not None:
-            new_argv += ['--fps', str(args.fps)]
-        if args.source is not None:
-            new_argv += ['--source', args.source]
-        if args.camera_index is not None:
-            new_argv += ['--camera-index', str(args.camera_index)]
-
-        sys.argv = new_argv
-        collect.main()
+    if not webcam_is_available():
+        print("No Webcam found.")
     else:
-        if not webcam_is_available():
-            print("No Webcam found.")
-        else:
-            print("Webcam found")
-            print("Super Mario Motion started")
+        print("Webcam found")
+        print("Super Mario Motion started")
 
-            state_manager = StateManager()
+        state_manager = StateManager()
 
-            vision.init()
-            vision_ml.init()
-            input.init()
-            gui.init()
+        vision.init()
+        vision_ml.init()
+        input.init()
+        gui.init()
 
-            gui.window.after(0, update)
-            gui.window.mainloop()
+        gui.window.after(0, update)
+        gui.window.mainloop()
