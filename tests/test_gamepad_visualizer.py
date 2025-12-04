edaaf912f3c7e8dcbc260845b52b1c7924853fb3
TimoB05 <timo.barton@student.uni-tuebingen.de>
2025-12-04 17:28:01 +0100

    added test for `gamepad_visualizer.py`

--- tests/test_gamepad_visualizer.py
+++ tests/test_gamepad_visualizer.py
@@ -2,9 +2,12 @@ import os
 
 from PIL import Image
 
+import super_mario_motion.gamepad_visualizer as gv
 from super_mario_motion import path_helper as ph
 from super_mario_motion.gamepad_visualizer import get_base_image, \
     pose_to_buttons
+
+last_orientation: str = "right"
 
 
 def images_equal(image1, image2):
@@ -28,9 +31,9 @@ def test_get_base_image():
 
 def test_pose_to_buttons():
     assert pose_to_buttons("standing") == []
-    _last_orientation = "right"
+    gv.last_orientation = "right"
     assert pose_to_buttons("jumping") == ["A", "DPAD_RIGHT"]
-    _last_orientation = "left"
+    gv.last_orientation = "left"
     assert pose_to_buttons("jumping") == ["A", "DPAD_LEFT"]
     assert pose_to_buttons("throwing") == ["B"]
     assert pose_to_buttons("running_right") == ["DPAD_RIGHT", "B"]
@@ -38,8 +41,31 @@ def test_pose_to_buttons():
     assert pose_to_buttons("running_left") == ["DPAD_LEFT", "B"]
     assert pose_to_buttons("walking_left") == ["DPAD_LEFT"]
     assert pose_to_buttons("walking_right") == ["DPAD_RIGHT"]
-    assert pose_to_buttons(_) == []
+    # TODO: add test for swimming
+    assert pose_to_buttons("not_defined") == []
 
 
 def test_draw_highlight_overlay():
-    pass
+    image = Image.new('RGBA', (200, 100))
+
+    res_image1 = gv.create_gamepad_image(
+        pose="walking_right",
+        send_active=False,
+        base_image_=image
+        )
+
+    res_image2 = gv.create_gamepad_image(
+        pose="walking_right",
+        send_active=True,
+        base_image_=image
+        )
+
+    assert image.mode == 'RGBA'
+    assert res_image1.size == image.size
+
+    expected = image.convert("RGB")
+
+    # image should remain the same because no inputs are sent
+    assert images_equal(res_image1, expected)
+    # image should be different because an input is being sent and highlighted
+    assert not images_equal(res_image2, expected)
