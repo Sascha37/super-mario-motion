59b04079fb9b76e6197e4bfae902d29925c542f9
Sascha <156184267+Sascha37@users.noreply.github.com>
2025-12-27 16:23:30 +0100

    Extended metrics for execution time and accuracy

--- src/super_mario_motion/metrics.py
+++ src/super_mario_motion/metrics.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+import glob
+import random
 import numpy as np
 from joblib import load
 
@@ -112,23 +114,48 @@ def get_execution_time(func,input):
     t_stop = time.perf_counter()
     return (t_stop - t_start)*1000
 
-
-def get_average_execution_time(runs, func, input):
+# Average execution time across multiple runs, each time loading in a random numpy array
+def get_average_execution_time(runs, func, npy_dir):
+    files = glob.glob(os.path.join(npy_dir,"**","*.npy"), recursive=True)
+    if not files:
+        raise ValueError(f"[Metrics] no npy files found in: {npy_dir}")
     total_time = 0
-    for n in range(runs):
-        total_time += get_execution_time(func, input)
+    for _ in range(runs):
+        file = random.choice(files)
+        input_data = transform_landmarks(np.load(file))
+        execution_time = get_execution_time(func, input_data)
+        total_time += execution_time
     return total_time / runs
 
-def get_accuracy(expected, dataset):
+# Calculates the average accuracy for determening a specific label
+def get_accuracy_for_label(expected, npy_dir, verbose):
+    files = glob.glob(os.path.join(npy_dir,f"{expected}","*.npy"))
+    if not files:
+        raise ValueError(f"[Metrics] no {expected}-npy files found in: {npy_dir}")
     correct = 0
-    for i,data in enumerate(dataset):
+    for file in files:
+        data = transform_landmarks(np.load(file))
+        if verbose:
+            print(file)
+            print(guess_most_likely(data))
         if (guess_most_likely(data) == expected):
             correct += 1
-    return (correct/(i+1))*100
+    return (correct/len(files))*100
+
+def get_total_accuracy(labels, npy_dir):
+    total_accuracy = 0
+    for label in labels:
+        total_accuracy += get_accuracy_for_label(label,npy_dir,False)
+    return (total_accuracy/len(labels))
+
+npy_path = ph.resource_path(
+                os.path.join("..", "..", "tests", "npy")
+                )
+
+#print_probability(sample_crouching)
+print(f"Average execution time: {get_average_execution_time(99, guess_most_likely, npy_path)} ms")
+print(f"Acc standing:{get_accuracy_for_label("standing", npy_path, False)}")
+print(f"Acc walking_left:{get_accuracy_for_label("walking_left", npy_path, True)}")
+print(f"Combined accuracy:{get_total_accuracy(["standing", "walking_left"],npy_path)}")
 
 
-print_probability(sample_crouching)
-print(f"Average execution time: {get_average_execution_time(99, guess_most_likely, sample_crouching)} ms")
-print(f"Acc:{get_accuracy("crouching", [sample_crouching,sample_crouching])}")
-
-print(f"Execution time: {get_execution_time(guess_most_likely, sample_crouching)}")
